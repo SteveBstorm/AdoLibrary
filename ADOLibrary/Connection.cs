21b24a0da6a7e1af342ea13cc2c102ec9f911c75
steveBstorm <steve.lorent@bstorm.be>
2021-08-26 09:59:55 +0200

    complete

--- ADOLibrary/Connection.cs
+++ ADOLibrary/Connection.cs
@@ -27,32 +27,88 @@ namespace ADOLibrary
 
         public int ExecuteNonQuery(Command cmd)
         {
-            throw new NotImplementedException();
+            using(DbConnection dbConnection = CreateConnection())
+            {
+                using(DbCommand sqlCommand = CreateCommand(cmd, dbConnection))
+                {
+                    dbConnection.Open();
+                    return sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
 
         public object ExecuteScalar(Command cmd)
         {
-            throw new NotImplementedException();
+            using (DbConnection dbConnection = CreateConnection())
+            {
+                using (DbCommand sqlCommand = CreateCommand(cmd, dbConnection))
+                {
+                    dbConnection.Open();
+                    object o = sqlCommand.ExecuteScalar();
+                    return (o is DBNull) ? null : o;
+                }
+            }
         }
 
-        public IEnumerable<TResult> ExecuteReader<TResult>(Command cmd)
-        {
-            throw new NotImplementedException();
-        }
+        //public IEnumerable<TResult> ExecuteReader<TResult>(Command cmd)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public IEnumerable<TResult> ExecuteReader<TResult>(Command cmd, Func<IDataRecord, TResult> converter)
+        public IEnumerable<TResult> ExecuteReader<TResult>(Command cmd, Func<IDataReader, TResult> converter)
         {
-            throw new NotImplementedException();
+            using(DbConnection connection = CreateConnection())
+            {
+                using(DbCommand sqlCommand = CreateCommand(cmd, connection))
+                {
+                    connection.Open();
+                    using(IDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            yield return converter(dataReader);
+                        }
+                    }
+                }
+            }
         }
 
         public DataTable GetDataTable(Command cmd)
         {
-            throw new NotImplementedException();
+            using (DbConnection connection = CreateConnection())
+            {
+                using (DbCommand sqlCommand = CreateCommand(cmd, connection))
+                {
+                    using (DbDataAdapter dataAdapter = _factory.CreateDataAdapter())
+                    {
+                        dataAdapter.SelectCommand = sqlCommand;
+                        DataTable dataTable = new DataTable();
+                        dataAdapter.Fill(dataTable);
+
+                        return dataTable;
+                    }
+                    
+                }
+            }
         }
 
         public DataSet GetDataSet(Command cmd)
         {
-            throw new NotImplementedException();
+            using (DbConnection connection = CreateConnection())
+            {
+                using (DbCommand sqlCommand = CreateCommand(cmd, connection))
+                {
+                    using (DbDataAdapter dataAdapter = _factory.CreateDataAdapter())
+                    {
+                        dataAdapter.SelectCommand = sqlCommand;
+                        DataSet dataSet = new DataSet();
+                        dataAdapter.Fill(dataSet);
+
+                        return dataSet;
+                    }
+
+                }
+            }
         }
 
         private DbConnection CreateConnection()
